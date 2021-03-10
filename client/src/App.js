f2d68da0c24ff1c7bb2727a2be0c05722706ab61
Somelina Obiechina <ifeasome@gmail.com>
2021-03-09 21:31:57 -0600

    initial firebase setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Welcome from "./pages/Welcome"
+import Welcome from "./pages/Welcome";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
 function App() {
   return (
