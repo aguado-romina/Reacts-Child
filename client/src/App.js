4a11216b15e327924d6e4cbaf908ca0667534c6f
Somelina Obiechina <ifeasome@gmail.com>
2021-03-11 13:31:01 -0600

    react and local server running

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,25 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import firebase from "firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import { Component } from "react";
 
-function App() {
-  return (
-    <Router>
-      <Welcome exact path="/" component={Welcome}/>
-    </Router>
-  );
+class App extends Component {
+  state={ isSignedIn: false }
+  render() {
+    return (
+      <div className="App"> 
+      {this.state.isSignedIn ? 
+      <div>Signed In!</div>
+      :
+      <div>Not Signed In!</div>
+       }
+      </div>
+    //   <Router>
+    //   <Welcome exact path="/" component={Welcome}/>
+    // </Router>
+    );
+  }
+  
 }
 
 export default App;
