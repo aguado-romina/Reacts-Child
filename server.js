292caad1e0b6db1a3042bea9b138a0d1d1547e2e
Romina Aguado <aguado.romi@gmail.com>
2021-03-11 20:00:21 -0600

    fixed server issue

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -21,4 +23,11 @@ app.get("*", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pet", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
