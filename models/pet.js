8bf10954940bc45a65cb1dbbd7ee952a6c9811b1
jessparnow <71057611+jessparnow@users.noreply.github.com>
2021-03-13 13:57:18 -0600

    trying again

--- models/pet.js
+++ models/pet.js
@@ -2,33 +2,31 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const petSchema = new Schema ({
-    petInfo: [
-       {
-           puppyParent: {
-               type: String,
-               required: true,
-           },
-           puppyName: {
-               type: String,
-               required: true,
-           },
-           puppyImage: {
-               type: String,
-               required: true,
-           },
-           breed: {
-               type: String, 
-               required: true,
-           },
-           age: {
-               type: String,
-               required: true,
-           }
-           
-       }
-    ]
-
+const petSchema = new Schema({
+  petInfo: [
+    {
+      puppyParent: {
+        type: String,
+        required: true,
+      },
+      puppyName: {
+        type: String,
+        required: true,
+      },
+      puppyImage: {
+        type: String,
+        required: true,
+      },
+      breed: {
+        type: String,
+        required: true,
+      },
+      age: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
 
 const Puppy = mongoose.model("Puppy", petSchema);
