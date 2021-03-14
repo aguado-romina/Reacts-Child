dd2542a346d466f3821e25f01716352487f59760
Mariam Ali <alim12@csp.edu>
2021-03-13 23:37:58 -0600

    more fixing

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { AuthContext } from "./AuthContext";
 
 const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
