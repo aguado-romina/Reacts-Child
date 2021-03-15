842165d53176f318833d1842f32cc9a7ef1d1e29
jessparnow <71057611+jessparnow@users.noreply.github.com>
2021-03-14 20:18:58 -0500

    adding some of the routes and the welcome page

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,14 +1,37 @@
-// the landing page 
+// the landing page
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Welcome () {
-    return (
-        <div>
-            <h1> Hello World </h1>
+function Welcome() {
+  return (
+    <div className="relative bg-white overflow-hidden">
+
+      <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+        <div className="sm:text-center lg:text-left">
+          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+            <span className="block xl:inline">Welcome</span>
+            <span className="block text-indigo-600 xl:inline">
+              to Doggy Pet App
+            </span>
+          </h1>
+          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+            An app designed to find your dog's perfect match.
+          </p>
         </div>
-    )
+        <div>
+            <button>
+            <Link
+              to="/signup"
+              className={
+                window.location.pathname === "/" || window.location.pathname === "/signup"}
+            >Sign Up</Link>
+            </button>
+        </div>
+      </main>
+    </div>
+  );
 }
 export default Welcome;
 
-//one sentence welcome tagline 
+//one sentence welcome tagline
 //Login/ sign Up button
