3913b17497ba9d6e97df3f20a98426596138b215
Romina Aguado <aguado.romi@gmail.com>
2021-03-16 19:23:43 -0500

    working on uploading img functionality

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const express = require("express");
+const cloudinary = require("cloudinary");
+const formData = require("express-form-data");
+const cors = require("cors");
+const { CLIENT_ORIGIN } = require("./config");
+
+const app = express();
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -30,4 +38,31 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pet", {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
+});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
+
+app.use(formData.parse());
+
+app.get("/wake-up", (req, res) => res.send("👌"));
+
+app.post("/image-upload", (req, res) => {
+  const values = Object.values(req.files);
+  const promises = values.map((image) =>
+    cloudinary.uploader.upload(image.path)
+  );
+
+  Promise.all(promises)
+    .then((results) => res.json(results))
+    .catch((err) => res.status(400).json(err));
 });
