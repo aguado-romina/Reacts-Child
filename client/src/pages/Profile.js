7e7b7b22cc4e363d7506271a36a3778f1e2ff339
jessparnow <71057611+jessparnow@users.noreply.github.com>
2021-03-19 20:26:48 -0500

    signout button working on navbar

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,34 +1,30 @@
 // where users can update their info
-// maybe upload photos 
+// maybe upload photos
 
-// where we could put the google maps api dog park 
+// where we could put the google maps api dog park
 // people can mark their favorite dog park and show it to other users or not
 
 import React from "react";
-import AuthApp from "../firebase";
-import {Redirect} from "react-router-dom";
+
+
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../AuthContext";
 import axios from "axios";
+import NavBar from "../components/NavBar";
 
 const Profile = ({ history }) => {
-  const {currentUser} = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
   useEffect(() => {
-    axios.get(`/api/profiles/${currentUser.uid}`)
-    .then((res) => {console.log(res.data)})
+    axios.get(`/api/profiles/${currentUser.uid}`).then((res) => {
+      console.log(res.data);
+    });
   }, []);
-  console.log(currentUser)
+  // console.log(currentUser);
   return (
     <>
-    <h1>Hello You made it!</h1>
-      <h1>Profile signOut</h1>
-            
-      <button onClick={() => {AuthApp.auth().signOut()
-      history.push("/")}}>Sign out</button>
-
+      <NavBar /> 
     </>
   );
 };
-
 
 export default Profile;
