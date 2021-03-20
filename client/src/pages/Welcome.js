0f3ed1aed98a68ddc4d423fd0ee51c9b7dd08ac0
Chavelyo3 <roxette.banos@gmail.com>
2021-03-19 20:37:00 -0500

    need new stuff

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 
 function Welcome() {
   return (
-    <div className="relative bg-white overflow-hidden md:flex md:justify-center mb-6">
-      
+    <div className="bg-white overflow-hidden md:flex md:justify-center mb-6">
+      <div class="font-montserrat"></div>
 
       <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-        <div className="sm:text-center lg:text-left">
+        <div className="sm:text-center lg:text-center">
           <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
             <span className="block xl:inline">Welcome</span>
             <span className="block text-indigo-600 xl:inline">
@@ -23,12 +23,14 @@ function Welcome() {
 
             </span>
           </h1>
+          <div className="flex flex-wrap justify-center">
           <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
             A letter written from a dog to their human.
 
           </p>
+          </div>
         </div>
-        <div>
+        <div className="flex flex-wrap justify-center">    
             <button>
             <Link
               to="/signup"
