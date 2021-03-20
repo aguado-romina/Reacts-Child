87a0451f97b1e57b17b0ea586d59ffce2b8250fb
Chavelyo3 <roxette.banos@gmail.com>
2021-03-20 13:12:29 -0500

    added color pallet and welcome changes

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -6,12 +6,13 @@ import { Link } from "react-router-dom";
 
 function Welcome() {
   return (
-    <div className="bg-white overflow-hidden md:flex md:justify-center mb-6">
+   
+    <div className="container bg-white  px-8 mx-auto rounded-lg">
       <div class="font-montserrat"></div>
 
-      <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+      <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-6 xl:mt-28">
         <div className="sm:text-center lg:text-center">
-          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl justify-center">
             <span className="block xl:inline">Welcome </span>
             <span className="block text-indigo-600 xl:inline">
               to Paw's Date
@@ -25,32 +26,33 @@ function Welcome() {
 
             </span>
           </h1>
-          <div className="flex flex-wrap justify-center">
+          <div className="flex justify-center">
           <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
             A letter written from a dog to their human.
 
           </p>
           </div>
         </div>
-        <div className="grid grid-cols-6">
+        <div className="flex flex-wrap justify-center">
+        
             <button
               type="submit"
-              className="group relative flex justify-center py-2 px-4 border border-transparent text-sm text-white font-medium rounded-md bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              className="group relative flex justify-center py-2 px-4 border border-transparent text-sm text-white font-medium rounded-md bg-lightblue hover:bg-darkblue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lightblue ">
                       
             <Link
-              to="/signup"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/signup"}
+               to="/signup"
+              // className={
+              //   window.location.pathname === "/" || window.location.pathname === "/signup"}
             >Sign Up</Link>
             </button>
             <button
              type="submit"
-             className="group relative flex justify-center py-2 px-4 border border-transparent text-sm text-white font-medium rounded-md bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+             className="group relative flex justify-center py-2 px-4 border border-transparent text-sm text-white font-medium rounded-md bg-lightblue hover:bg-darkblue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-lightblue ">
                      
             <Link
               to="/login"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/login"}
+              // className={
+              //   window.location.pathname === "/" || window.location.pathname === "/login"}
             >Login</Link>
             </button>
         </div>
