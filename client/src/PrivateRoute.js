2588832756afdd35410ab3dc7d635798e84d2ca4
Somelina Obiechina <ifeasome@gmail.com>
2021-03-22 20:16:02 -0500

    delete button added

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -1,24 +1,39 @@
 import React, { useContext } from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import { AuthContext } from "./AuthContext";
 
-const PrivateRoute = ({ component: RouteComponent, ...rest }) => {
+const PrivateRoute = ({ children, path }) => {
   const {currentUser} = useContext(AuthContext);
   return (
+    <>
     <Route
-      {...rest}
-      render={routeProps =>
-        !!currentUser ? (
-          <RouteComponent {...routeProps} />
-        ) : (
-          <>
-          </>
-          // <Redirect to={"/Login"} /> (took out the redirect)
-        )
-      }
+      
+        { ...!!currentUser ? (
+          <Route path={path}>{children}</Route>
+        ) : ( 
+          <Redirect to="/login" /> 
+        )}
     />
+</>
   );
 };
+// import React, { useContext } from 'react';
+// import { Route, Redirect } from 'react-router-dom';
+
+// import AuthContext from '../context/AuthContext';
+
+// const PrivateRoute = ({ children, path }) => {
+//   const { currentUser } = useContext(AuthContext);
+//   return (
+//     <>
+//       {currentUser.isLoggedIn ? (
+//         <Route path={path}>{children}</Route>
+//       ) : (
+//         <Redirect to="/" />
+//       )}
+//     </>
+//   );
+// };
 
 
 export default PrivateRoute;
